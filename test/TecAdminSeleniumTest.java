2a24570c214708d75ac1439d8a61f1a810c4e920
mdwansar <47854535+mdwansar@users.noreply.github.com>
2019-05-20 01:17:04 +0530

    Update TecAdminSeleniumTest.java

--- test/TecAdminSeleniumTest.java
+++ test/TecAdminSeleniumTest.java
@@ -1,25 +1,30 @@
 import java.io.IOException;
- 
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
- 
+import org.openqa.selenium.By;
+
 public class TecAdminSeleniumTest {
- 
+
         public static void main(String[] args) throws IOException, InterruptedException {
                 System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
                 ChromeOptions chromeOptions = new ChromeOptions();
                 chromeOptions.addArguments("--headless");
                 chromeOptions.addArguments("--no-sandbox");
- 
+
                 WebDriver driver = new ChromeDriver(chromeOptions);
- 
+        //      driver = new ChromeDriver();
+
                 driver.get("http://13.234.71.156:3001/");
- 
+        //      driver.manage().window().maximize();
+        //      driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+
                 Thread.sleep(1000);
- 
-                if (driver.getPageSource().contains("About Us")) {
+                driver.findElement(By.id("About Us")).click();
+
+                if (driver.getPageSource().contains("about")) {
                         System.out.println("Pass");
                 } else {
                         System.out.println("Fail");
